6fb03858853f854528688050bca8056cf6454e1d
Diego <diegoluizjava.java@gmail.com>
2018-02-13 21:00:06 -0300

    Add leaflet map

--- gas-pwa/src/pages/home/home.ts
+++ gas-pwa/src/pages/home/home.ts
@@ -11,7 +11,7 @@ export class HomePage {
   current: any = MapPage;
 
   constructor(public navCtrl: NavController) {
-
+    
   }
 
 
--- gas-pwa/src/pages/map/map.ts
+++ gas-pwa/src/pages/map/map.ts
@@ -1,5 +1,8 @@
 import { NavController } from 'ionic-angular';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import leaflet from 'leaflet';
+
+const RECIFE = [-8.0403523, -34.9037189];
 
 @Component({
     selector: 'page-map',
@@ -7,7 +10,32 @@ import { Component } from '@angular/core';
 })
 export class MapPage {
 
+    @ViewChild('map') mapContainer: ElementRef;
+    map: any;
+
     constructor(public navController: NavController) {
+
+    }
+
+    ionViewDidEnter() {
+        this.loadMap();
+        // console.log(this.map);
+    }
+
+    loadMap() {
+
+        let tilelayerOptions = {
+            maxZoom: 18,
+            detectRetina: true
+        }
+
+        let tileLayer = leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', tilelayerOptions);
+        
+        let mapOptions = { zoomControl: false, center: RECIFE, zoom: 12 };
+
+        this.map = leaflet.map("map", mapOptions);
+
+        tileLayer.addTo(this.map);
 
     }
 
